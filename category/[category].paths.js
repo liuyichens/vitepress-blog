264753701667f68f83eed4b0d16d3245c77e4187
liuyichen <2643150172@qq.com>
2023-12-06 19:38:44 +0800

    deploy

--- category/[category].paths.js
+++ category/[category].paths.js
@@ -2,12 +2,12 @@ import { loadPosts } from "../utils/loadPosts"
 
 export default {
   async paths() {
-    const allPosts = await loadPosts()
-    const allCategories = [...new Set(allPosts.map(post => post.category).flat())]
-    return allCategories.map(c => ({ params: { category: c }}))
-    // return [
-    //   { params: { category: 'foo' }},
-    //   { params: { category: 'bar' }}
-    // ]
+    // const allPosts = await loadPosts()
+    // const allCategories = [...new Set(allPosts.map(post => post.category).flat())]
+    // return allCategories.map(c => ({ params: { category: c }}))
+    return [
+      { params: { category: 'foo' }},
+      { params: { category: 'bar' }}
+    ]
   }
 }
